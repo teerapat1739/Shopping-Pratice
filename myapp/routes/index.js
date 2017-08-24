d7a979a2a71e7850cb850931df098d172eaba251
teerapat <teerapat12345678@gmail.com>
2017-08-25 02:07:33 +0700

    create signup.hbs and config index.hbs and index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Product =require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shopping Cart' });
+  Product.find(function(err, docs){
+    //var productChunks=[];
+  //  var chunkSize=3;
+  //  for(var i; i<docs.length; i+=chunkSize){
+    //  productChunks.push(docs.slice(i, i+chunkSize));
+  // }
+      res.render('shop/index', { title: 'Shopping cart', products: docs });// successMsg ถูกนำไป render ที่ index.hbs
+
+  });
 });
 
 module.exports = router;
